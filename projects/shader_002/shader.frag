87959cc7731e2a5ae62517671326acd2df606b8a
tobiasras <tobiasras.job@gmail.com>
2026-06-10 23:25:39 +0200

    added n body shader

--- projects/shader_002/shader.frag
+++ projects/shader_002/shader.frag
@@ -11,27 +11,32 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 
 
+uniform vec3 u_repel_orbs[20];
+//uniform int u_repel_orb_count;
+
+
 
 void main() {
     // Aspect-correct UV: origin stays at canvas center for any aspect ratio
     vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / u_resolution.y;
-    vec2 mouse = (u_mouse * 2.0 - u_resolution.xy) / u_resolution.y;
 
-    float pulse = sin(u_time*2.)*0.2+0.55;
-
-
-    float pixel_d = length(uv);
-    pixel_d = (0.05*pulse) / pixel_d;
-
-    float d = distance(uv, mouse);
-
-    float ball = u_scroll / d;
+    vec3 color = vec3(3,2,1);
     
-    vec3 color = vec3(1, 2, 3);
-    
-    color *= ball + pixel_d;
 
-    
+
+    for (int i = 0; i < 20; i++) {
+        vec3 obj = u_repel_orbs[i];
+
+        float x = obj.x;
+        float y = obj.y;
+        float radius = obj.z;
+
+        float d = distance(uv, vec2(x, y));
+
+        float ball = 0.4 / d+0.005 + 0.2;
+
+        color *= ball;
+    }
 
     gl_FragColor = vec4(color, 1.0);
 }
--- projects/shader_002/sketch.js
+++ projects/shader_002/sketch.js
@@ -1,4 +1,5 @@
 
+
 let shaderProgram;
 let frameCounter = 0;
 
@@ -9,33 +10,40 @@ min_size = 0
 
 
 
+const env = {
+  params: {
+    repel_orb_count: 20
+  },
+  orbs: [],
+  gravity: 10,
+  center_strenth: 0.2
+
+}
+
+
+
 function preload() {
   shaderProgram = loadShader('shader.vert', 'shader.frag');
 }
 
 function canvasSize() {
-  const vv = window.visualViewport;
-  if (vv) {
-    return [Math.round(vv.width), Math.round(vv.height)];
-  }
   return [windowWidth, windowHeight];
 }
 
 function setup() {
   pixelDensity(1);
-
   const [w, h] = canvasSize();
   const canvas = createCanvas(w, h, WEBGL);
   canvas.parent('sketch-container');
-
-  noStroke();
-
+  noStroke()
   // setup listerners
   addEventListener("wheel", onMouseWheelScroll)
-
   if (window.visualViewport) {
     window.visualViewport.addEventListener('resize', onViewportChange);
   }
+
+  // Sets env and all data needed for sketch
+  init()
 }
 
 function onViewportChange() {
@@ -45,18 +53,31 @@ function onViewportChange() {
 
 // runs all the time
 function draw() {
+  let dt = deltaTime / 100000;
+  update(dt)
+
+  
   shader(shaderProgram);
 
   let time_val = millis() * 0.001;
-
+  
   shaderProgram.setUniform('u_time', time_val);
   shaderProgram.setUniform('u_scroll', size);
-
   shaderProgram.setUniform('u_resolution', [width, height]);
   shaderProgram.setUniform('u_mouse', [mouseX, height - mouseY]);
 
-  rect(-width / 2, -height / 2, width, height);
 
+  const objectData = [];
+  for (const obj of env.orbs) {
+    objectData.push(obj.x);
+    objectData.push(obj.y);
+    objectData.push(obj.mass);
+  }
+  shaderProgram.setUniform('u_repel_orbs', objectData);
+
+
+  
+  rect(-width / 2, -height / 2, width, height);
   frameCounter++;
 }
 
@@ -66,17 +87,7 @@ function windowResized() {
 }
 
 
-
-
-
-
-
-
-
-
-
 function onMouseWheelScroll(event) {
-  console.log(size)
   const movement = event.deltaY
   let dir = 0;
   if (movement < 0) {
@@ -85,12 +96,101 @@ function onMouseWheelScroll(event) {
     dir = -1
   }
   const force = size_increment * dir
-  
   const result = force + size
   if (result < min_size || result > max_size ){
     return
   }
   size += force
 }
+
+
+function init() {
+  const orb_count = env.params.repel_orb_count;
+  const padding = 0.2;
+
+
+
+
+  for (let i = 0; i < orb_count; i++) {
+    const orb = { 
+      x: Math.random() * (2 - 2 * padding) - (1 - padding),
+      y: Math.random() * (2 - 2 * padding) - (1 - padding),
+      mass: 0.005, 
+      accelerations_x: 0,
+      accelerations_y: 0,
+      velocity_x : 0,
+      velocity_y : 0,
+      force_x: 0,
+      force_y: 0
+    }; 
+      env.orbs.push(orb) 
+    }
+
+    console.log(env)
+}
+
+
+function update(dt) {
+
+  compute_force()
+  update_orbs(dt)
+}
+
+function compute_force() {
+  for (const orb of env.orbs) {
+    orb.force_x = 0;
+    orb.force_y = 0;
+  }
+
+for (let i = 0; i < env.orbs.length; i++) {
+  for (let j = i + 1; j < env.orbs.length; j++) {
+
+
+
+      const dx = env.orbs[j].x - env.orbs[i].x
+      const dy = env.orbs[j].y - env.orbs[i].y
+
+      const r2 =  dx * dx + dy * dy
+      const r = Math.sqrt(r2)
+
+      let F = env.gravity * env.orbs[i].mass * env.orbs[j].mass /r2;
+
+      const Fx = F * dx / r;
+      const Fy = F * dy / r;
+
+      env.orbs[i].force_x += Fx;
+      env.orbs[i].force_y += Fy;
+      env.orbs[j].force_x -= Fx;
+      env.orbs[j].force_y -= Fy;
+      
+
+      env.orbs[i].force_x  += -env.center_strenth * env.orbs[i].x;
+      env.orbs[i].force_y += -env.center_strenth * env.orbs[i].y;
+      env.orbs[j].force_x  += -env.center_strenth * env.orbs[j].x;
+      env.orbs[j].force_y += -env.center_strenth * env.orbs[j].y;
+
+
+
+
+    }
+  }
+}
+
+function update_orbs(dt) {
+  
+
+
+  for (const orb of env.orbs) {
+    orb.accelerations_x = orb.force_x / orb.mass;
+    orb.accelerations_y = orb.force_y / orb.mass;
+
+    orb.velocity_x += orb.accelerations_x * dt;
+    orb.velocity_y += orb.accelerations_y * dt;
+
+    orb.x += orb.velocity_x * dt;
+    orb.y += orb.velocity_y * dt;
+  }
+}
+
 
 
